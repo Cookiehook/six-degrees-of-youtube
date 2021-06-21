a348465c4711fa3b7f7975db1a9571e8869d2e9b
Cookiehook <shaun.harrison@live.co.uk>
2021-06-21 08:54:24 +0100

    Removed debug file caching

--- src/models/youtube_object.py
+++ src/models/youtube_object.py
@@ -1,4 +1,3 @@
-import json
 import os
 from copy import copy
 
@@ -12,15 +11,6 @@ class YoutubeObject:
 
     @staticmethod
     def get(endpoint, params):
-        if os.getenv('DEBUG'):
-            # Get from cached responses rather than API
-            filename = f"{endpoint}_{'_'.join([f'{k}-{v}' for k, v in params.items()])}.json".replace("|", "")
-            filepath = os.path.join('data', 'api-queries', filename)
-            if os.path.exists(filepath):
-                with open(filepath, "r") as queryfile:
-                    response = json.loads(queryfile.read())
-                    return response.get('items'), response.get('nextPageToken')
-
         print(f"Querying API for '{endpoint}' with parameters '{params}")
         base_url = os.getenv('YOUTUBE_API_URL', 'https://www.googleapis.com/youtube/v3/')
 
@@ -38,8 +28,4 @@ class YoutubeObject:
         if 'items' not in response.json() or len(response.json()['items']) == 0:
             raise HTTPError('API responded with no items')
 
-        if os.getenv("DEBUG"):
-            # Write responses to file cache
-            with open(filepath, "w") as queryfile:
-                queryfile.write(json.dumps(response.json(), indent=2))
         return response.json().get('items'), response.json().get('nextPageToken')
