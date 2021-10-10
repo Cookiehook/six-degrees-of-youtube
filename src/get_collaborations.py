45e64fe8eced64f9d5f5d73adcf594d779361bb4
Cookiehook <shaun.harrison@live.co.uk>
2021-10-10 22:55:57 +0100

    Error handling added. Threading now split by video, not channel

--- src/get_collaborations.py
+++ src/get_collaborations.py
@@ -1,5 +1,7 @@
 import datetime
 import json
+import math
+import os
 import threading
 
 from requests import HTTPError
@@ -52,14 +54,14 @@ def get_target_channel(channel_name: str) -> Channel:
     return Channel.from_id(match[0].id)
 
 
-def build_channel_cache(target_channel, channel_cache):
+def build_channel_cache(uploads, channel_cache):
     channel_ids = set()
     channel_titles = set()
     channel_urls = set()
     channel_usernames = set()
     video_ids = set()
 
-    for video in Video.from_channel(target_channel):
+    for video in uploads:
         channel_ids.update(video.get_channel_ids_from_description())
         channel_titles.update(video.get_channel_titles_from_title())
         channel_urls.update(video.get_urls_from_description())
@@ -67,93 +69,131 @@ def build_channel_cache(target_channel, channel_cache):
         video_ids.update(video.get_video_ids_from_description())
 
     for _ in channel_ids:
-        channel_cache.add(Channel.from_id(_))
+        try:
+            channel_cache.add(Channel.from_id(_))
+        except HTTPError:
+            print(f"Failed retrieving channel for id: {_}")
 
     for _ in channel_titles:
         if not channel_cache.by_title(_):
-            channel_cache.add(get_channel_from_title(channel_cache, _))
+            try:
+                channel_cache.add(get_channel_from_title(channel_cache, _))
+            except HTTPError:
+                print(f"Failed retrieving channel for title: {_}")
 
     for _ in channel_urls:
         if not channel_cache.by_url(_):
-            channel_cache.add(Channel.from_url(_))
+            try:
+                channel_cache.add(Channel.from_url(_))
+            except HTTPError:
+                print(f"Failed retrieving channel for URL: {_}")
 
     for _ in channel_usernames:
-        new_channel = Channel.from_username(_)
-        # Username can be queried, but isn't returned by the API
-        # This if statement saves the username in the cache, allowing later reuse
-        if old_channel := channel_cache.by_id(new_channel.id):
-            old_channel.username = _
-        else:
-            channel_cache.add(new_channel)
+        if not channel_cache.by_username(_):
+            try:
+                new_channel = Channel.from_username(_)
+                # Username can be queried, but isn't returned by the API
+                # This if statement saves the username in the cache, allowing later reuse
+                if old_channel := channel_cache.by_id(new_channel.id):
+                    old_channel.username = _
+                else:
+                    channel_cache.add(new_channel)
+            except HTTPError:
+                print(f"Failed retrieving channel for username: {_}")
 
     for _ in video_ids:
-        video = Video.from_id(_)
-        if not channel_cache.by_id(video.channel_id):
-            channel_cache.add(Channel.from_id(video.channel_id))
+        try:
+            video = Video.from_id(_)
+            if not channel_cache.by_id(video.channel_id):
+                channel_cache.add(Channel.from_id(video.channel_id))
+        except HTTPError:
+            print(f"Failed retrieving video for ID: {_}")
 
 
-def get_collaborations(target_channel: Channel, channel_cache: ChannelCache, out_collaborations: dict):
-    out_collaborations[target_channel] = {}
+def get_collaborations(uploads: list, channel_cache: ChannelCache, out_collaborations: dict):
+    for video in uploads:
+        host_channel = channel_cache.by_id(video.channel_id)
+        if out_collaborations.get(host_channel) is None:
+            out_collaborations[host_channel] = {}
 
-    for video in Video.from_channel(target_channel):
-        if video.id == "jH-FK2RmZBo":
-            print()
         for channel_id in video.get_channel_ids_from_description():
             if channel := channel_cache.by_id(channel_id):
-                if channel == target_channel:
+                if channel == host_channel:
                     continue
-                if out_collaborations[target_channel].get(channel) is None:
-                    out_collaborations[target_channel][channel] = []
-                out_collaborations[target_channel][channel].append(video)
+                if out_collaborations[host_channel].get(channel) is None:
+                    out_collaborations[host_channel][channel] = []
+                out_collaborations[host_channel][channel].append(video)
 
         for possible_title in video.get_channel_titles_from_title():
             pass  # TODO - Implement
 
         for url in video.get_urls_from_description():
             if channel := channel_cache.by_url(url):
-                if channel == target_channel:
+                if channel == host_channel:
                     continue
-                if out_collaborations[target_channel].get(channel) is None:
-                    out_collaborations[target_channel][channel] = []
-                out_collaborations[target_channel][channel].append(video)
+                if out_collaborations[host_channel].get(channel) is None:
+                    out_collaborations[host_channel][channel] = []
+                out_collaborations[host_channel][channel].append(video)
 
         for username in video.get_users_from_description():
             if channel := channel_cache.by_username(username):
-                if channel == target_channel:
+                if channel == host_channel:
                     continue
-                if out_collaborations[target_channel].get(channel) is None:
-                    out_collaborations[target_channel][channel] = []
-                out_collaborations[target_channel][channel].append(video)
+                if out_collaborations[host_channel].get(channel) is None:
+                    out_collaborations[host_channel][channel] = []
+                out_collaborations[host_channel][channel].append(video)
 
         for video_id in video.get_video_ids_from_description():
             try:
                 linked_video = Video.from_id(video_id)
                 if channel := channel_cache.by_id(linked_video.channel_id):
-                    if channel == target_channel:
+                    if channel == host_channel:
                         continue
-                    if out_collaborations[target_channel].get(channel) is None:
-                        out_collaborations[target_channel][channel] = []
-                    out_collaborations[target_channel][channel].append(video)
+                    if out_collaborations[host_channel].get(channel) is None:
+                        out_collaborations[host_channel][channel] = []
+                    out_collaborations[host_channel][channel].append(video)
             except HTTPError:
                 print(f"Failed to find linked video {video_id} from video {video}")
+
+
+def get_uploads(channel: Channel, uploads: list):
+    uploads.extend(Video.from_channel(channel))
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 
 def lambda_handler(event, context):
     print(datetime.datetime.now())
     channel_cache = ChannelCache()
     target_channel = get_target_channel(event['target_channel'])
-    build_channel_cache(target_channel, channel_cache)
+    channel_cache.add(target_channel)
+    build_channel_cache(Video.from_channel(target_channel), channel_cache)
 
-    threads = []
-    collabs = {}
-
+    all_uploads = []
+    upload_threads = []
     for channel in channel_cache.cache:
-        t = threading.Thread(target=get_collaborations,
-                             args=(channel, channel_cache, collabs))
+        t = threading.Thread(target=get_uploads,
+                             args=(channel, all_uploads))
         t.start()
-        threads.append(t)
+        upload_threads.append(t)
+    for thread in upload_threads:
+        thread.join()
 
-    for thread in threads:
+    collab_threads = []
+    collabs = {}
+    num_threads = os.getenv("NUM_THREADS", 200)
+
+    for chunk in chunks(all_uploads, math.ceil(len(all_uploads) / num_threads)):
+        t = threading.Thread(target=get_collaborations,
+                             args=(chunk, channel_cache, collabs))
+        t.start()
+        collab_threads.append(t)
+
+    for thread in collab_threads:
         thread.join()
 
     print(datetime.datetime.now())
